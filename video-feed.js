723d89c8518d891b3eab29fd74fd5dd8ab3a9ea3
Mithru Vigneshwara <mithru@users.noreply.github.com>
2021-04-01 09:25:52 -0400

    video feed update

--- video-feed.js
+++ video-feed.js
@@ -1,11 +1,14 @@
-var video = document.querySelector("#videoElement");
-
-if (navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices.getUserMedia({ video: true })
-    .then(function (stream) {
-      video.srcObject = stream;
-    })
-    .catch(function (err) {
-      console.log("Something went wrong! " + err);
-    });
+function start(){
+  video = document.querySelector("#videoElement");
+  console.log(video)
+  if (navigator.mediaDevices.getUserMedia) {
+    console.log(video)
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then(function (stream) {
+        video.srcObject = stream;
+      })
+      .catch(function (err) {
+        console.log("Something went wrong! " + err);
+      });
+  }
 }
