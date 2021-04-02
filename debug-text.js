af36f000a11a57a5d5f831508b8a133acb56fbe9
Mithru Vigneshwara <mithru@users.noreply.github.com>
2021-04-02 10:49:46 -0400

    Add files via upload

--- debug-text.js
+++ debug-text.js
@@ -8,6 +8,7 @@ AFRAME.registerComponent('debug-this', {
       this.camera = document.getElementById('camera')
       this.origin = this.camera.object3D.position.clone()
       this.portalElements = document.getElementById('all-content')
+      this.dashboardElements = document.getElementById('dashboard-content')
       this.portalElementsOrigin = this.portalElements.object3D.position.clone()
       this.hasEntered = false
       this.mainContent = document.getElementById('main1')
@@ -15,6 +16,7 @@ AFRAME.registerComponent('debug-this', {
       this.walls = document.getElementById('hider-walls')
       this.portalWall = document.getElementById('portal-wall')
       // this.contents.object3D.visible = false
+      this.dashboardElements.object3D.visible = false
     },
     tick() {
       // const moveX = Math.abs(this.origin.x - this.camera.object3D.position.x)
@@ -37,6 +39,8 @@ AFRAME.registerComponent('debug-this', {
         const isInside = (dist < 9.5)
         this.debugTextElement.innerHTML = dist
         if (isInside) {
+
+          this.dashboardElements.object3D.visible = true
           this.hasEntered = true
           this.walls.object3D.visible = false
           this.portalWall.object3D.visible = false
